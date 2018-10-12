e76bfd267c2b4016f0793f11a56669e495105e0a
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-10-12 14:10:42 +0300

    Status for element, color and type of selection.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 $(document).ready(function() {
-  function update_status(text) {
-    $("p#status").text(text);
+  var selected_element;  // dom element - table cell
+  var selected_color;    // hex: #EEAEEA
+  var option;            // cell, row or column
+
+  function update_status(text, logger) {
+    // Update text for element, color or type
+    if(logger == "element") {
+      $("p#status-element").text(text);
+      return;
+    }
+    if(logger == "color") {
+      $("p#status-color").text(text);
+      return;
+    }
+    if(logger == "type") {
+      $("p#status-type").text(text);
+    }
   }
 
   $('table#editor').click(function(e) {
-    update_status("You selected: " + e.target.textContent);
+    update_status("Element selected: " + e.target.textContent, "element");
   });
 
   $("#select-color").spectrum({
     color: "#f00",
     change: function(color) {
-        $("#basic-log").text("change called: " + color.toHexString());
+      update_status("Color selected: " + color.toHexString(), "color");
     }
   });
 

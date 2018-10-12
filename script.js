328645303d2d031fec056bb9a5447dba65f3a8df
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-10-12 13:09:49 +0300

    Selected item.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+  function update_status(text) {
+    $("p#status").text(text);
+  }
+
+  $('table#editor').click(function(e) {
+    update_status("You selected: " + e.target.textContent);
+  });
   // alert("Hello");
 });

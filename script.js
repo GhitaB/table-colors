e252cfdc64f095c8d0e78d2b6f405a946add6e9f
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-10-12 13:37:22 +0300

    Add spectrum color picker jquery plugin.

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ $(document).ready(function() {
   $('table#editor').click(function(e) {
     update_status("You selected: " + e.target.textContent);
   });
-  // alert("Hello");
+
+  $("#select-color").spectrum({
+    color: "#f00",
+    change: function(color) {
+        $("#basic-log").text("change called: " + color.toHexString());
+    }
+  });
+
 });
